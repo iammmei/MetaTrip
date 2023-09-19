e4e99307b7e90cd3b0765470cd39b1280816ed6c
Safa <safae.khess@gmail.com>
2023-09-19 15:45:29 +0200

    navbar i

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,25 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faBars } from '@fortawesome/free-solid-svg-icons';
+import {  faBars,faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
   
 function Navbar() {
+  const [click,setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
       <>
    <nav className='navbar'>
       <div className="navbar-container">
           <Link to = "/" className="navbar-logo">
               TRAVEL 
-  <FontAwesomeIcon icon={faBars} />
+
 
           </Link>
-          <div className="menu-icon">
-
+          <div className="menu-icon" onClick={handleClick}>
+          <FontAwesomeIcon icon={click? faTimes : faBars} /> </div> 
           </div>
-
-      </div>
   </nav>
 
 
