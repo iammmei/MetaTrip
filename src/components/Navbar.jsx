8be1f4b3f84933ff96be814eb13abbef78d4ac3c
Safa <safae.khess@gmail.com>
2023-09-21 10:08:40 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faBars,faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Button } from './Button';
-
+import './Navbar.css';
 
   
 function Navbar() {
