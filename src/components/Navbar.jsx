3c4fa1455e227a868f7ed35b299f999057325641
Safa <safae.khess@gmail.com>
2023-09-21 09:48:54 +0200

    button jsw/css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,31 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faBars,faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Button } from './Button';
 
 
   
 function Navbar() {
   const [click,setClick] = useState(false);
+  const [button,setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  return (
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    
+    }
+    else{
+      setButton(true)
+    }
+  };
+  window.addEventListener('resize',showButton);
+
+
+
+    return (
       <>
    <nav className='navbar'>
       <div className="navbar-container">
@@ -50,6 +66,7 @@ function Navbar() {
         </li>
 
         </ul>
+        {button && <Button buttonStyle= 'btn--outline'>Sign UP </Button>}
 
         </div>
   </nav>
