e66bba5db004be2f828bc8e3e84596cadc8d92b9
Safa <safae.khess@gmail.com>
2023-09-21 11:09:56 +0200

    home creation

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../App.css'
+import { Button } from './Button'
+import './HeroSection.css'
+import {  faPlay } from '@fortawesome/free-solid-svg-icons';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+        <video src="/videos/video-2.mp4" autoPlay loop muted></video>
+      <h1>ADVENTURE WAITS</h1>
+      <p> What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button 
+        className="btns"
+         buttonStyle='btn--outline'
+        buttonSize='btn--large'
+        >
+            GET STARTED 
+        </Button>
+
+        <Button 
+        className="btns"
+         buttonStyle='btn--primary'
+        buttonSize='btn--large'
+        > WATCH TRAILER <FontAwesomeIcon icon={faPlay} /> 
+    </Button>
+
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
