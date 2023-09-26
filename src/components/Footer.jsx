b7b6e7f4f7a73db13f756df96bbd65430872112f
Safa <safae.khess@gmail.com>
2023-09-26 15:28:47 +0200

    creating footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+            Join the adventures newsletter to receive our best vacations deals
+        </p>
+        <p className="footer-subscription-text">
+            Yu
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default Footer
