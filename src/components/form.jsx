79fc2c65023bf4ec226748b8850768b415289762
Safa <safae.khess@gmail.com>
2023-10-02 09:18:08 +0200

    ...

---
+++ src/components/form.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import '../../App.css';
+import './SignUp.css'
+
+export default function SignUp() {
+
+
+
+    const [formData, setFormData] = useState({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    });
+  
+  
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
+  
+    // Function to handle form submission
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    
+      console.log('Form submitted:', formData);
+    };
+  
+    return (
+      <>
+    
+        <h1 className='sign-up'>Sign Up</h1>
+        <div className="container">
+        <form className='form' onSubmit={handleSubmit}>
+          <label>
+            First Name:
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+  
+          <label>
+            Last Name:
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+  
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+  
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+  
+          <button type="submit">Sign Up</button>
+        </form>
+        </div>
+      </>
+    );
+  }
+  
