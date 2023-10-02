110cffbc182f48a6e85c1ef8bfb74cc945add4ba
Safa <safae.khess@gmail.com>
2023-10-02 09:29:37 +0200

    ..

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import '../../App.css';
-import './SignUp.css'
+import './Form.css'
 
-export default function SignUp() {
+export default function Form() {
 
 
 
